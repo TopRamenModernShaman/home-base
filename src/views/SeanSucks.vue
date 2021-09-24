e63e752c8033d51e5fcc62791af6687d9f0c6653
TopRamenModernShaman <46510446+TopRamenModernShaman@users.noreply.github.com>
2021-09-24 17:12:11 -0500

    sean

---
+++ src/views/SeanSucks.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+      <p @click="kanye"> SEAN SUCKS </p>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+
+export default {
+  name: 'Home',
+  data() {
+      return{
+				quote: ''
+      }
+  },
+  components: {
+  },
+  methods: {
+		async kanye(){
+			fetch('https://api.kanye.rest')
+				.then(response => response.json())
+				.then(data => console.log(data));
+		}
+  }
+}
+</script>
