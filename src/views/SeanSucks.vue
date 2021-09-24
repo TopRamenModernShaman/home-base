800ebf7d7bb33b6b5759434309446732a41ec1a0
TopRamenModernShaman <46510446+TopRamenModernShaman@users.noreply.github.com>
2021-09-24 17:33:02 -0500

    mtg

--- src/views/SeanSucks.vue
+++ src/views/SeanSucks.vue
@@ -1,6 +1,18 @@
 <template>
   <div>
       <p @click="kanye"> SEAN SUCKS </p>
+			<span>{{quote}}</span>
+			<br />
+			<br />
+			<br />
+			<br />
+			<br />
+			<br />
+			<span class="mtgcard">Phyrexian Dreadnought</span> for existing. But really <span class="mtgcard">Serra Angel [M10]</span>
+
+			<blockquote>
+    		12 Serra Angel
+			</blockquote>
   </div>
 </template>
 
@@ -16,12 +28,21 @@ export default {
   },
   components: {
   },
+	created() {
+		this.kanye()
+	},
   methods: {
 		async kanye(){
 			fetch('https://api.kanye.rest')
 				.then(response => response.json())
-				.then(data => console.log(data));
+				.then(data => this.quote = data.quote);
 		}
   }
 }
 </script>
+<style scoped>
+p {
+	cursor: pointer;
+	color: orange;
+}
+</style>
